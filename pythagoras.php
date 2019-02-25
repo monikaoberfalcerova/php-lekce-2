a16cf8f70fcbea4fc3fdddf116a8fc1aa9ae5317
monikaoberfalcerova <monika.oberfalcerova@seznam.cz>
2019-02-25 19:38:01 +0100

    Přidání trojúhelníku

--- pythagoras.php
+++ pythagoras.php
@@ -4,3 +4,16 @@ $b = 5;
 $obdelnikobsah = $a * $b;
 echo "Obsah obdélníku o stranách $a cm a $b cm je $obdelnikobsah cm².";
 ?>
+
+<?php
+echo '<br>';
+echo '<br>';
+?>
+
+<?php
+$strana = 6;
+$uhel = 60;
+$vyska = $strana * sin(deg2rad ($uhel));
+$trojuhelnikobsah = round ($vyska * $strana / 2);
+echo "Rovnostranný trojúhelník o straně délky $strana cm a úhlu $uhel stupňů je $trojuhelnikobsah cm².";
+?>
