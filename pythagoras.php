58f9ddb80b1393221b5e309b554752538d9b5861
monikaoberfalcerova <monika.oberfalcerova@seznam.cz>
2019-02-26 23:41:34 +0100

    Pythagoras - hezká stránka

--- pythagoras.php
+++ pythagoras.php
@@ -1,19 +1,69 @@
-<?php
-$a = 4;
-$b = 5;
-$obdelnikobsah = $a * $b;
-echo "Obsah obdélníku o stranách $a cm a $b cm je $obdelnikobsah cm².";
-?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
 
-<?php
-echo '<br>';
-echo '<br>';
-?>
+      <!-- Bootstrap core CSS -->
+      <link href="css/bootstrap.min.css" rel="stylesheet">
 
-<?php
-$strana = 6;
-$uhel = 60;
-$vyska = $strana * sin(deg2rad ($uhel));
-$trojuhelnikobsah = round ($vyska * $strana / 2);
-echo "Rovnostranný trojúhelník o straně délky $strana cm a úhlu $uhel stupňů je $trojuhelnikobsah cm².";
-?>
+      <!-- Custom styles for this template -->
+      <link href="starter-template.css" rel="stylesheet">
+
+
+  </head>  <body style="color: black; background-color: lightcyan;">...</body>
+
+
+  <body>
+
+      <div class="media">
+          <img class="mr-6" src="https://u.smedata.sk/blogidnes/article/9/42/422509/422509_clanok_foto_254.jpg?r=27bq" onload="typeof google==='object'&amp;&amp;google.aft&amp;&amp;google.aft(this)" alt="Výsledek obrázku pro pythagoras obrázek" style="margin-top: 51px;" data-iml="1551212340611" width="167" height="252">
+          <div class="media-body">
+              <br>
+              <br>
+              <h2 class="col-md-6">Pythagoras</h2>
+              <br>
+              <p>Pythagoras ze Samu byl řecký filosof, matematik, astronom i kněz. Narodil na ostrově Samos.</p>
+              <p>V mládí cestoval po Egyptě a Babylonii, kde se seznámil s východními náboženskými myšlenkami.</p>
+              <p>Založil dva stupně škol, v Egyptě a kolem roku 530 př. n. l. v Krotónu v Itálii pythagorejskou školu.</p>
+              <p>Jeho žáci mu připisovali objev vztahu mezi obsahy čtverců u stran pravoúhlého trojúhelníka,</p>
+              <p>dnes vztah známý jako Pythagorova věta.</p>
+              <br>
+              <br>
+              <div class="media mt-3">
+                  <a class="mr-6" href="#">
+                      <img src="https://matematika.cz/content/images/obd1.png" class="mr-3" alt="Obrázek obdélník">
+                  </a>
+                  <div class="media-body">
+                      <h3 class="mt-0">Obdélník</h3>
+
+                      <p> a = 4 cm </p>
+                      <p> b = 5 cm </p>
+                      <p> S = ab </p>
+                      <p> O = 2a + 2b </p>
+                      <p> Obsah obdélníku o stranách 4 cm a 5 cm je 20 cm². </p>
+                  </div>
+              </div>
+
+                      <br>
+
+                      <div class="media mt-3">
+                          <a class="mr-6" href="#">
+                              <img alt="VlastnostiRovnostrannehoTrojuhelnika.jpg" src="//upload.wikimedia.org/wikipedia/commons/1/1a/VlastnostiRovnostrannehoTrojuhelnika.jpg" decoding="async" data-file-width="386" data-file-height="251" width="386" height="251">                      </a>
+                      <div class="media-body">
+                          <h3 class="mt-0">Rovnostranný trojúhelník</h3>
+                               <p>a = 6 cm</p>
+                              <p>úhel = 60 stupňů</p>
+                              <p>S = a · v / 2 = a2 · √3  / 4</p>
+                              <p>O = 3a</p>
+                              <p>v = a * sin α</p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+    </body>
+</html>
